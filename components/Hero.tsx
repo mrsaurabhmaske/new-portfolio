6c2a96bff0c3644665902e4863633c531a0f4048
mrsaurabhmaske <saurabh.r.maske@gmail.com>
2025-12-07 17:33:33 +0530

    Added theme based BG Glows on hero page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,28 +2,27 @@ import Link from "next/link";
 import styles from "./Hero.module.scss";
 
 export default function Hero() {
-    return (
-        <div className={styles.heroContainer}>
-            {/* Background Glows */}
-            <div className={styles.glowPurple} />
-            <div className={styles.glowBlue} />
+  return (
+    <div className={styles.heroContainer}>
+      {/* Background Glows */}
+      <div className={styles.glowPrimary} />
+      <div className={styles.glowSecondary} />
 
-            <h1 className={styles.heading}>
-                Hi, I'm <span className={styles.nameGradient}>Saurabh Maske</span>
-            </h1>
-            <p className={styles.description}>
-                Frontend Engineer specializing in React & Next.js.
-            </p>
+      <h1 className={styles.heading}>
+        Hi, I'm <span className={styles.nameGradient}>Saurabh Maske</span>
+      </h1>
+      <p className={styles.description}>
+        Frontend Engineer specializing in React & Next.js.
+      </p>
 
-            <div className={styles.actions}>
-                <Link href="/projects" className={styles.btnProject}>
-                    View Projects
-                </Link>
-                <Link href="/contact" className={styles.btnContact}>
-                    Contact Me
-                </Link>
-            </div>
-        </div>
-    );
+      <div className={styles.actions}>
+        <Link href="/projects" className={styles.btnProject}>
+          View Projects
+        </Link>
+        <Link href="/contact" className={styles.btnContact}>
+          Contact Me
+        </Link>
+      </div>
+    </div>
+  );
 }
-
